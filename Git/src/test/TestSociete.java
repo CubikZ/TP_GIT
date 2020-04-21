b8b837d225550baf7b262967c95460cd5d1265d9
Leon Khenniche <leon.khenniche@outlook.fr>
2020-04-21 19:04:46 +0200

    creation de la classe TestSociete

---
+++ Git/src/test/TestSociete.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import entree.*;
+
+/**
+ *
+ * @author leon
+ */
+public class TestSociete {
+    final static Societe societe = new Societe("societe1");
+    
+    public static void main(String[] args){
+        
+        System.out.println(societe.getRaisonSociale());
+        
+        societe.setRaisonSociale("societe2");
+        
+        System.out.println(societe.toString());
+        
+    }   
+    
+}
+
