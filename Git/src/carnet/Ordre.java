dbebeeeb8a5dbacdaa158fc8942f68745046ace7
Leon Khenniche <leon.khenniche@outlook.fr>
2020-04-22 11:27:45 +0200

    creation Ordre et Carnet

---
+++ Git/src/carnet/Ordre.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carnet;
+
+/**
+ *
+ * @author leon
+ */
+public enum Ordre {
+    CROISSANT,
+    DECROISSANT;  
+}
